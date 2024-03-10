23281900488f332837213e96814c9abecede2e0d
TAGZIRIA Sara <sara.tagziria@soprasteria.com>
2024-03-10 19:47:17 +0100

    #1: add constants

--- src/main/java/yatzy/score/commun/score/utils/Constants.java
+++ src/main/java/yatzy/score/commun/score/utils/Constants.java
@@ -7,16 +7,66 @@ package yatzy.score.commun.score.utils;
  * 
  */
 public class Constants {
-	
-    /** Dice faces **/
-	public static final int NUMBER_ONE_OF_DICE = 1;
-    public static final int NUMBER_TWO_OF_DICE = 2;
-    public static final int NUMBER_THREE_OF_DICE = 3;
-    public static final int NUMBER_FOUR_OF_DICE = 4;
-    public static final int NUMBER_FIVE_OF_DICE = 5;
-    public static final int NUMBER_SIX_OF_DICE = 6;
 
-    /** dice score **/
-    public static final int YATZY_SCORE = 50;
+	//Dice faces
+	/** Represents the face value of one on a dice **/
+	public static final int DICE_SIDE_ONE = 1;
+	/** Represents the face value of two on a dice **/
+	public static final int DICE_SIDE_TWO = 2;
+	/** Represents the face value of three on a dice **/
+	public static final int DICE_SIDE_THREE = 3;
+	/** Represents the face value of four on a dice **/
+	public static final int DICE_SIDE_FOUR = 4;
+	/** Represents the face value of five on a dice **/
+	public static final int DICE_SIDE_FIVE = 5;
+	/** Represents the face value of six on a dice **/
+	public static final int DICE_SIDE_SIX = 6;
+	/** Represents the total number of sides on a dice **/
+	public static final int DICE_NUMBER_SIDES = 6;
+	/** Represents the starting index for dice arrays **/
+	public static final int DICE_START_INDEX = 0;
+	/** Represents the ending index for dice arrays **/
+	public static final int DICE_END_INDEX = 6;
+
+	//Dice score 
+	/** Represents a Yatzy score of 0 **/
+	public static final int YATZY_SCORE_0 = 0;
+	/** Represents a Yatzy score of 15 **/
+	public static final int YATZY_SCORE_15 = 15;
+	/** Represents a Yatzy score of 50 **/
+	public static final int YATZY_SCORE_50 = 50;
+	/** Represents a Yatzy score of 20 **/
+	public static final int YATZY_SCORE_20 = 20;
+
+
+	//Dice kind
+	/** Rerensents three occurences **/
+	public static final int THREE_OCCURRENCES = 3;
+	/** Rerensents four occurences **/
+	public static final int FOUR_OCCURRENCES = 4;
+
+	//index
+	/** Represents start index on 0 **/
+	public static final int INDEX_START_TO_ZERO = 0;
+	/** Represents start index on 1 **/
+	public static final int INDEX_START_TO_ONE = 1;
+	/** Represents end index on 5 **/
+	public static final int INDEX_END_TO_FIVE = 5;
+	/** Represents end index on 4 **/
+	public static final int INDEX_END_TO_FOUR = 4;
+	
+	//Occurrences array
+	/** Represents the number of 2 pairs **/
+	public static final int NUMBER_OF_TWO_PAIR = 2;
+	/** Represents single pair **/
+	public static final int NUMBER_OF_ONE_PAIR = 1;
+	/** Represents a single occurrence**/
+	public static final int ONE_OCCURRENCE = 1;
+	/** Represents two occurrences **/
+	public static final int TWO_OCCURRENCES = 2;
+
+
+
+
 
 }
